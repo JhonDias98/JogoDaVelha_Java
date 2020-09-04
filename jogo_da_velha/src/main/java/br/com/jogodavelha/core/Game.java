47199aa76338f6610a6dedc2c256655134e5d37b
Jonathan Dias <jonathan.dias.p2017@gmail.com>
2020-09-03 23:31:46 -0300

    Lógica do próximo jogador

--- jogo_da_velha/src/main/java/br/com/jogodavelha/core/Game.java
+++ jogo_da_velha/src/main/java/br/com/jogodavelha/core/Game.java
@@ -7,13 +7,37 @@ public class Game {
 	
 	private Board board = new Board();
 	private Player[] players = new Player[Constants.SYMBOL_PLAYERS.length];
+	private int currentPlayerIndex = 0;
 	
 	public void play() {
 		UI.printGameTitle();
 		
-		board.clear();
-		board.print();
+		for(int i = 0; i < players.length; i++) {
+			players[i] = createPlayer(i);
+		}
+	}
+	
+	private Player createPlayer(int index) {
+		String name = UI.readInput("Jogador " + (index + 1) + " =>");
+		char symbol = Constants.SYMBOL_PLAYERS[index];
+		Player player = new Player(name, board, symbol);
 		
-		//UI.readInput("Nome do Jogador");
+		UI.printText("O jogador " + name+ " vai usar o símbolo " + symbol);
+		
+		return player;
+	}
+	
+	private Player nextPlayer() {
+		//Forma mais detalhada de fazer
+		/*
+		currentPlayerIndex++;
+		if(currentPlayerIndex >= players.length) {
+			currentPlayerIndex = 0;
+		}
+		*/
+		
+		//Forma menos detalhada de fazer
+		currentPlayerIndex = (currentPlayerIndex + 1) % players.length;
+		return players[currentPlayerIndex];
 	}
 }
