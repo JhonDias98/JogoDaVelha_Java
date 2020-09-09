d08a4be66bdeb9972bbed078de3a8ac7f16e6c0e
Jonathan Dias <jonathan.dias.p2017@gmail.com>
2020-09-09 00:12:29 -0300

    checando se a jogada é permitida.

--- jogo_da_velha/src/main/java/br/com/jogodavelha/core/Board.java
+++ jogo_da_velha/src/main/java/br/com/jogodavelha/core/Board.java
@@ -50,13 +50,19 @@ public class Board {
 		return true;
 	}
 	
-	public boolean play(Player player, Move move) {
+	public boolean play(Player player, Move move) throws InvalidMoveException {
 		int i = move.getI();
 		int j = move.getJ();
 		
+		if(i < 0 || j < i || i>= Constants.BOARD_SIZE || j >= Constants.BOARD_SIZE) {
+			throw new InvalidMoveException("O intervalo da jogada é inválido");
+		}
+		
+		if(matrix[i][j] != ' ') {
+			throw new InvalidMoveException("Esta jogada já foi realizada");
+		}
+		
 		matrix[i][j] = player.getSymbol();
-		
-		
 		return checkRows(player) || checkCols(player) 
 				|| checkDiagonal1(player) || checkDiagonal2(player);
 	}
